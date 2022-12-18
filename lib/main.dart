dabdcd2d13fdda59e83705076e3f318228cdcd39
Tigran Ghazinyan <me@tigranes.io>
2022-12-18 11:57:41 -0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:manuscript_reader_flutter/manuscript_reader_app.dart';
+
+void main() {
+  runApp(const ManuscriptReaderApp());
+}
+
---
+++ lib/screens/book_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class BookScreen extends StatefulWidget {
+  const BookScreen({super.key});
+
+  @override
+  State<BookScreen> createState() => _BookScreenState();
+}
+
+class _BookScreenState extends State<BookScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
